9a7b8e0385700d791d8eab0ab887c9e60f7f1558
arseniycz <162355569+arseniycz@users.noreply.github.com>
2024-03-21 20:45:33 +0100

    Update scriptClick.js

--- Zadani/scriptClick.js
+++ Zadani/scriptClick.js
@@ -6,22 +6,22 @@ function changeColor(element) {
         clickCount = 0;
     }
 
-    // Получаем случайные значения RGB для цвета
+  
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
 
-    // Устанавливаем цвет фона выбранного элемента
+    
     element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     element.style.border = `5px solid rgb(${green}, ${red}, ${blue})`;
 
 
-    // Увеличиваем счетчик кликов
+    
     clickCount++;
 
     element.setAttribute("clickCount", clickCount);
 
-    // Если кликов было пять, меняем цветы всех элементов на черный и сбрасываем счетчик
+   
     if (clickCount === 5) {
         element.style.backgroundColor = 'black';
         clickCount = 0;
