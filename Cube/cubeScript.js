611dd80e31fed24b7728fbc68043fe43eb2143b8
arseniycz <162355569+arseniycz@users.noreply.github.com>
2024-03-28 19:38:43 +0100

    Pres Chrome

---
+++ Cube/cubeScript.js
@@ -0,0 +1,127 @@
+document.addEventListener('mousemove', e => {
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
+		`
+	})
+})
+
+
+
+function createCube(size) {
+    let layer3 = document.querySelector('.layer-3');
+    let cube = document.createElement('div');
+
+    cube.classList.add('cube' );
+    cube.style.width = size + 'px';
+    cube.style.height = size + 'px';
+    //cube.style.backgroundImage = url(img/bok1.png);
+    document.body.appendChild(cube);
+    layer3.appendChild(cube);
+    return cube;
+}
+
+function createCubeBack(size) {
+    let cubeBack = document.createElement('div');
+    cubeBack.classList.add('cubeBack' );
+    cubeBack.style.width = size + 'px';
+    cubeBack.style.height = size + 'px';
+    //cube.style.backgroundImage = url(img/bok1.png);
+    document.body.appendChild(cubeBack);
+    return cubeBack;
+}
+
+
+
+function colorCube(cube, color) {
+    cube.style.backgroundColor = color, + '0.1';
+}
+
+function addBorderRadius(cube, radius) {
+    cube.style.borderRadius = radius + 'px';
+}
+
+
+let myCube = createCube(220);
+colorCube(myCube, '..');
+addBorderRadius(myCube, 0);
+myCube.classList.add('left');
+
+// let myCubeBack = createCubeBack(240);
+// colorCube(myCubeBack, '..');
+// addBorderRadius(myCubeBack, 10);
+// myCubeBack.classList.add('left');
+
+let myCube2 = createCube(220);
+colorCube(myCube2, '..');
+addBorderRadius(myCube2, 15);
+myCube2.classList.add('right');
+
+
+     
+
+
+function makeCube(element) {
+
+    let clickCount = element.getAttribute("clickCount");
+    if(typeof(clickCount) === undefined)
+    {
+        clickCount = 0;
+    }
+
+    // Получаем случайные значения RGB для цвета
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    // Устанавливаем цвет фона выбранного элемента
+    element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    element.style.border = `5px solid rgb(${green}, ${red}, ${blue})`;
+    element.style.background = '110%'; 
+
+    
+    clickCount++;
+
+    element.setAttribute("clickCount", clickCount);
+
+   
+    if (clickCount === 3) {
+        element.style.backgroundColor = 'black';
+        clickCount = 0;
+        element.setAttribute("clickCount", clickCount);
+        const element2 = document.querySelector('.layer-3 img');
+        element2.style.height = '120vh';
+        const element3 = document.querySelector('.colorMix');
+        element3.style.opacity = '100%';
+        element.style.display = 'none';
+        
+        
+        
+    }
+}
+
+
+function colorLeftCube(changer) {
+    const red1 = Math.floor(Math.random() * 256);
+     const green1 = Math.floor(Math.random() * 256);
+     const blue1 = Math.floor(Math.random() * 256);
+
+    const changer1 = document.querySelector('.left');
+    changer1.style.backgroundColor = `rgb(${red1}, ${green1}, ${blue1})`;
+    const element4 = document.querySelector('.left');
+        element4.style.display = 'flex';
+    
+}
+
+function colorRightCube(changer) {
+
+    const red2 = Math.floor(Math.random() * 256);
+     const green2 = Math.floor(Math.random() * 256);
+     const blue2 = Math.floor(Math.random() * 256);
+    const changer2 = document.querySelector('.right');
+    changer2.style.backgroundColor = `rgb(${red2}, ${green2}, ${blue2})`;
+    const element5 = document.querySelector('.right');
+        element5.style.display = 'flex';
+    
+}
