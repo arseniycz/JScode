82ca5fd18198abe213cf9a1427c419d3ea58b58b
arseniycz <162355569+arseniycz@users.noreply.github.com>
2024-04-30 13:02:01 +0200

    Add files via upload

---
+++ ZadaniFinal/07Barvy/scriptClick.js
@@ -0,0 +1,32 @@
+function changeColor(element) {
+
+    let clickCount = element.getAttribute("clickCount");
+    if(typeof(clickCount) === undefined)
+    {
+        clickCount = 0;
+    }
+
+    
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    
+    element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    element.style.border = `5px solid rgb(${green}, ${red}, ${blue})`;
+
+
+ 
+    clickCount++;
+
+    element.setAttribute("clickCount", clickCount);
+
+
+    if (clickCount === 5) {
+        element.style.backgroundColor = 'black';
+        clickCount = 0;
+        element.setAttribute("clickCount", clickCount);
+        element.style.backgroundColor = 'black';
+        element.style.transform = 'rotateY(360deg)'; 
+    }
+}
